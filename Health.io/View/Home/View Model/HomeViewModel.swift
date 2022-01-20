6c01f9636678dc66515bf2bfbd9165f701d60525
oem66 <omer.6g@gmail.com>
2022-01-20 22:38:49 +0100

    add core files to home view model

---
+++ Health.io/View/Home/View Model/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  Health.io
+//
+//  Created by Omer Rahmanovic on 1/20/22.
+//
+
+import Foundation
+
+final class HomeViewModel {
+
+}
---
+++ Health.io/View/Home/View/HomeView.swift
@@ -0,0 +1,15 @@
+//
+//  HomeView.swift
+//  Health.io
+//
+//  Created by Omer Rahmanovic on 1/20/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+            .padding()
+    }
+}
