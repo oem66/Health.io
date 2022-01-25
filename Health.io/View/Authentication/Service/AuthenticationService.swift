191b4e6b3fc6f4bd9661532436e6f596dbb2a2af
oem66 <omer.6g@gmail.com>
2022-01-25 23:43:19 +0100

    Authentication Service

--- Health.io/View/Authentication/Service/AuthenticationService.swift
+++ Health.io/View/Authentication/Service/AuthenticationService.swift
@@ -46,6 +46,6 @@ final class AuthenticationService: AuthenticationServiceProtocol {
     }
     
     func observeAuthChange() -> AnyPublisher<User?, Never> {
-        Publishers.AuthenticationPublishers().eraseToAnyPublisher()
+        Publishers.AuthenticationPublisher().eraseToAnyPublisher()
     }
 }
