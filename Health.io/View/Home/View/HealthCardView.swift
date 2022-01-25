8158a04c83480a12f9894319dcd2e73351ea7d52
oem66 <omer.6g@gmail.com>
2022-01-25 19:21:33 +0100

    Home View structure

---
+++ Health.io/View/Home/View/HealthCardView.swift
@@ -0,0 +1,17 @@
+//
+//  HealthCardView.swift
+//  Health.io
+//
+//  Created by Omer Rahmanovic on 1/25/22.
+//
+
+import SwiftUI
+
+// MARK: Use this View to show all available health data
+struct HealthCardView: View {
+    var body: some View {
+        Text("Health Card View")
+            .foregroundColor(.black)
+            .padding()
+    }
+}
