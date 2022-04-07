dbf8b478a2e4ba6ad9e3ef495be0d0c8219f320f
oem66 <omer.6g@gmail.com>
2022-04-07 18:09:53 +0200

    add diet details view

---
+++ Health.io/View/Home/Diet View/DietDetailsView.swift
@@ -0,0 +1,17 @@
+//
+//  DietDetailsView.swift
+//  Health.io
+//
+//  Created by Omer Rahmanovic on 4/7/22.
+//
+
+import SwiftUI
+
+struct DietDetailsView: View {
+    var body: some View {
+        VStack {
+            Text("Diet View")
+                .padding()
+        }
+    }
+}
