e097b5065440272ece6cc701551bf4135e914052
oem66 <omer.6g@gmail.com>
2022-05-10 00:40:34 +0200

    add ingredient card view

--- Health.io/View/Tab Bar/Diet View/DietDetailsView.swift
+++ Health.io/View/Tab Bar/Diet View/DietDetailsView.swift
@@ -162,3 +162,54 @@ struct MainContentDietView: View {
         }
     }
 }
+
+struct IngredientCardView: View {
+    var body: some View {
+        ZStack {
+            Color(red: 246/255, green: 248/255, blue: 252/255)
+            HStack {
+                ZStack {
+                    Color(.white)
+                    VStack {
+                        Image(systemName: "cart.fill")
+                            .resizable()
+                            .frame(width: 20, height: 20, alignment: .center)
+                    }
+                    .padding(5)
+                }
+                .clipped()
+                .cornerRadius(10)
+                .padding(3)
+                
+                VStack {
+                    HStack {
+                        Text("Coconut Oil")
+                            .foregroundColor(.black)
+                            .font(.custom("Avenir-Medium", size: 18))
+                            .fontWeight(.heavy)
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Text("Cheap and easy to buy")
+                            .foregroundColor(.gray)
+                            .font(.custom("Avenir-Medium", size: 15))
+                            .fontWeight(.bold)
+                    }
+                }
+                
+                Spacer()
+                
+                VStack {
+                    Text("1 spoon")
+                        .font(.custom("Avenir-Medium", size: 15))
+                        .foregroundColor(.gray)
+                        .fontWeight(.bold)
+                }
+                .padding(3)
+            }
+        }
+        .clipped()
+        .cornerRadius(10)
+    }
+}
