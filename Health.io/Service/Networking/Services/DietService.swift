99d1714a5128d5d05aa0a657177e0e8fddf778e7
oem66 <omer.6g@gmail.com>
2022-06-13 23:07:59 +0200

    add diet service

---
+++ Health.io/Service/Networking/Services/DietService.swift
@@ -0,0 +1,74 @@
+//
+//  DietService.swift
+//  Health.io
+//
+//  Created by Omer Rahmanovic on 13. 6. 2022..
+//
+
+import Foundation
+import Combine
+
+protocol DietProtocol {
+    func getDietHealthTips() -> [DietHealthTipsModel]
+    func getFastingDiets() -> [DietPlanModel]
+    func getBalancedDiets() -> [DietPlanModel]
+    func getKetoLowCarbDiets() -> [DietPlanModel]
+    func getHighProteinDiets() -> [DietPlanModel]
+}
+
+final class DietService: DietProtocol {
+    
+    func getDietHealthTips() -> [DietHealthTipsModel] {
+        let dietHealthTips = [
+            DietHealthTipsModel(image: "food1", title: "Fasting", description: "16:8 fasting is very good for health"),
+            DietHealthTipsModel(image: "food2", title: "Loose Weight", description: "Loosing weight improves your health"),
+            DietHealthTipsModel(image: "food3", title: "Gain Muscle", description: "Muscle gain can be very good for your health"),
+            DietHealthTipsModel(image: "food4", title: "Meal Plan", description: "Complete and quality diet is great for health")
+        ]
+        return dietHealthTips
+    }
+    
+    func getFastingDiets() -> [DietPlanModel] {
+        let fastingDiets = [
+            DietPlanModel(image: "food1", title: "16:8 Morning Fasting", description: "21-day meal plan", dietPlan: ""),
+            DietPlanModel(image: "food2", title: "16:8 Evening Fasting", description: "21-day meal plan", dietPlan: ""),
+            DietPlanModel(image: "food3", title: "5:2 Fasting", description: "Fast 2 days/week", dietPlan: ""),
+            DietPlanModel(image: "food4", title: "6:1 Fasting", description: "Fast 1 day/week", dietPlan: ""),
+            DietPlanModel(image: "food2", title: "48h Fasting", description: "Fast 48h straight", dietPlan: ""),
+        ]
+        return fastingDiets
+    }
+    
+    func getBalancedDiets() -> [DietPlanModel] {
+        let balancedDiets = [
+            DietPlanModel(image: "food1", title: "16:8 Morning Fasting", description: "21-day meal plan", dietPlan: ""),
+            DietPlanModel(image: "food2", title: "16:8 Evening Fasting", description: "21-day meal plan", dietPlan: ""),
+            DietPlanModel(image: "food3", title: "5:2 Fasting", description: "Fast 2 days/week", dietPlan: ""),
+            DietPlanModel(image: "food4", title: "6:1 Fasting", description: "Fast 1 day/week", dietPlan: ""),
+            DietPlanModel(image: "food2", title: "48h Fasting", description: "Fast 48h straight", dietPlan: "")
+        ]
+        return balancedDiets
+    }
+    
+    func getKetoLowCarbDiets() -> [DietPlanModel] {
+        let ketoLowCarbDiets = [
+            DietPlanModel(image: "food1", title: "16:8 Morning Fasting", description: "21-day meal plan", dietPlan: ""),
+            DietPlanModel(image: "food2", title: "16:8 Evening Fasting", description: "21-day meal plan", dietPlan: ""),
+            DietPlanModel(image: "food3", title: "5:2 Fasting", description: "Fast 2 days/week", dietPlan: ""),
+            DietPlanModel(image: "food4", title: "6:1 Fasting", description: "Fast 1 day/week", dietPlan: ""),
+            DietPlanModel(image: "food2", title: "48h Fasting", description: "Fast 48h straight", dietPlan: ""),
+        ]
+        return ketoLowCarbDiets
+    }
+    
+    func getHighProteinDiets() -> [DietPlanModel] {
+        let highProteinDiets = [
+            DietPlanModel(image: "food1", title: "16:8 Morning Fasting", description: "21-day meal plan", dietPlan: ""),
+            DietPlanModel(image: "food2", title: "16:8 Evening Fasting", description: "21-day meal plan", dietPlan: ""),
+            DietPlanModel(image: "food3", title: "5:2 Fasting", description: "Fast 2 days/week", dietPlan: ""),
+            DietPlanModel(image: "food4", title: "6:1 Fasting", description: "Fast 1 day/week", dietPlan: ""),
+            DietPlanModel(image: "food2", title: "48h Fasting", description: "Fast 48h straight", dietPlan: ""),
+        ]
+        return highProteinDiets
+    }
+}
--- Health.io/View Model/Diet/DietViewModel.swift
+++ Health.io/View Model/Diet/DietViewModel.swift
@@ -6,12 +6,28 @@
 //
 
 import Foundation
+import Combine
+
+// THIS VIEW MODEL WILL HAVE SIMILAR STRUCTURE AS THIS ONE https://github.com/oem66/Buildify-/blob/master/Buildify/View%20Model/CreateChallengeViewModel.swift
 
 enum DietType {
-    case fasting, balanced, ketoLowCarb, highProtein
+    case fasting([DietPlanModel], String)
+    case balanced([DietPlanModel], String)
+    case ketoLowCarb([DietPlanModel], String)
+    case highProtein([DietPlanModel], String)
 }
 
-final class DietViewModel {
+final class DietViewModel: ObservableObject {
+    
+    // put enum inside view model
+    // create multiple services to fetch diet data
+    
+    private let service: DietProtocol
+    
+    init(service: DietProtocol = DietService()) {
+        self.service = service
+    }
+    
     var dietHealthTips = [
         DietHealthTipsModel(image: "food1", title: "Fasting", description: "16:8 fasting is very good for health"),
         DietHealthTipsModel(image: "food2", title: "Loose Weight", description: "Loosing weight improves your health"),
@@ -32,7 +48,7 @@ final class DietViewModel {
         DietPlanModel(image: "food2", title: "16:8 Evening Fasting", description: "21-day meal plan", dietPlan: ""),
         DietPlanModel(image: "food3", title: "5:2 Fasting", description: "Fast 2 days/week", dietPlan: ""),
         DietPlanModel(image: "food4", title: "6:1 Fasting", description: "Fast 1 day/week", dietPlan: ""),
-        DietPlanModel(image: "food2", title: "48h Fasting", description: "Fast 48h straight", dietPlan: ""),
+        DietPlanModel(image: "food2", title: "48h Fasting", description: "Fast 48h straight", dietPlan: "")
     ]
     
     var ketoLowCarbDiets = [
